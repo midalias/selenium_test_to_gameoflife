8903f5575a214e71a5ff0ab396262617c99d5234
midalias <idalia.solis@hotmail.com>
2015-09-30 14:34:51 -0600

    First commit

---
+++ src/test/java/com/akyzen/webdriver/newGameButton.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.akyzen.webdriver;
+
+import java.util.List;
+import junit.framework.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ *
+ * @author midalias
+ */
+public class newGameButton {
+    
+    public newGameButton() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void newGameButtonClicTest() {
+        WebDriver driver = new FirefoxDriver();
+        driver.navigate().to("http://54.69.10.130:8080/game-of-life/");
+            
+        WebElement newGameButton = driver.findElement(By.linkText("New Game"));
+        newGameButton.click();
+        
+        String strExpectedText = "Please seed your universe";
+        WebElement element = driver.findElement(By.id("seedUniverseDiv"));
+        String strElementText  = element.getText();
+        Assert.assertEquals(strExpectedText, strElementText);
+        
+        driver.close();        
+    }    
+}
---
+++ src/test/java/com/akyzen/webdriver/titleTest.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.akyzen.webdriver;
+/*
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+*/
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ *
+ * @author midalias
+ */
+public class titleTest {
+    
+    @Test
+    public void titleNameTest() {
+        WebDriver driver = new FirefoxDriver();
+        driver.navigate().to("http://54.69.10.130:8080/game-of-life/");
+
+        Assert.assertTrue("title should start differently", 
+                                                driver.getTitle().startsWith("The Game Of Life"));
+
+        driver.close();        
+    }
+    
+    /*
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    */
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
