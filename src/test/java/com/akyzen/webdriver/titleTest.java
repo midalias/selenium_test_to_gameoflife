beaccdeca5e1d581f718cb76d00159f84da801ca
midalias <idalia.solis@hotmail.com>
2016-08-08 16:29:57 -0600

    Update titleTest.java

--- src/test/java/com/akyzen/webdriver/titleTest.java
+++ src/test/java/com/akyzen/webdriver/titleTest.java
@@ -4,24 +4,11 @@
  * and open the template in the editor.
  */
 package com.akyzen.webdriver;
-/*
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 
-import junit.framework.Assert;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-*/
 import junit.framework.Assert;
 
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 /**
  *
@@ -31,36 +18,14 @@ public class titleTest {
     
     @Test
     public void titleNameTest() {
-        WebDriver driver = new FirefoxDriver();
-        driver.navigate().to("http://52.71.221.84:8080/game-of-life/");
-
+        // Declaring and initialising the HtmlUnitWebDriver
+        HtmlUnitDriver unitDriver = new HtmlUnitDriver();
+        unitDriver.navigate().to("http://52.71.221.84:8080/game-of-life/");
+        
         Assert.assertTrue("title should start differently", 
-                                                driver.getTitle().startsWith("The Game Of Life"));
+                                                unitDriver.getTitle().startsWith("The Game Of Life"));
 
-        driver.close();        
+        System.out.println("Title is: " + unitDriver.getTitle());
+        unitDriver.close();
     }
-    
-    /*
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-    */
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
