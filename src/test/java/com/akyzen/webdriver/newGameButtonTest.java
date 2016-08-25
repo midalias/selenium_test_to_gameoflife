26ece5737950cbc61ab7d5873f49a712a9b0d4b1
midalias <idalia.solis@hotmail.com>
2016-08-25 13:18:00 -0600

    Update newGameButtonTest.java

--- src/test/java/com/akyzen/webdriver/newGameButtonTest.java
+++ src/test/java/com/akyzen/webdriver/newGameButtonTest.java
@@ -34,7 +34,7 @@ public class newGameButtonTest {
     public void newGameButtonClicTest() {
         //WebDriver driver = new FirefoxDriver();
         HtmlUnitDriver unitDriver = new HtmlUnitDriver();
-        unitDriver.navigate().to("http://52.71.221.84:8080/game-of-life");
+        unitDriver.navigate().to("http://54.159.235.123:8080/game-of-life");
             
         WebElement newGameButton = unitDriver.findElement(By.linkText("New Game"));
         newGameButton.click();
